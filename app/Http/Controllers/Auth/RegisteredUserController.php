699bc9c0e5fe6e17f32fb8eac1e161ed31ee0a64
Ruben Freire Andrade <rubenswork20@gmail.com>
2023-10-14 10:45:41 +0100

    Changed behaviour to routing when registering.

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,10 +31,11 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        // TODO: Return original ruleset for Production.
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:'.User::class,
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed', /*Rules\Password::defaults()*/],
         ]);
 
         $user = User::create([
