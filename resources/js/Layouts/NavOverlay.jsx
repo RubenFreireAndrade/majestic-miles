5e050c0663c386209ed013b68f2f107f223e7bcb
Ruben Freire Andrade <rubenswork20@gmail.com>
2023-10-28 01:07:06 +0100

    Completed required NavBar & made into a Layout.

---
+++ resources/js/Layouts/NavOverlay.jsx
@@ -0,0 +1,51 @@
+import { Link} from '@inertiajs/react';
+import { BsFillTelephoneFill } from 'react-icons/bs';
+import { AiOutlineMail } from 'react-icons/ai';
+
+export default function NavBar({ auth }) {
+    return (
+        <>
+            {/* This would be a layout */}
+            <div class="p-4 flex flex-row justify-evenly items-center border border-solid border-black bg-blue-300">
+                <Link href={route('/')} class="m-1 font-bold underline">Majestic Miles</Link>
+
+                <div class="flex flex-col border border-solid border-black">
+                    <div class="flex flex-row">
+                        <AiOutlineMail/> <span> <a href="mailto:info.majesticmiles@gmail.com" class='mx-2 hover:bg-green-300'>info.majesticmiles@gmail.com</a> </span>
+
+                        <BsFillTelephoneFill/> <span class='mx-2 hover:bg-green-300'>90123908981</span>
+                    </div>
+                    <div class="flex flex-row justify-evenly">
+                        <>
+                            <Link href={route('services')} class='mx-1 hover:bg-green-300'>Services</Link>
+
+                            <Link href={route('about')} class='mx-1 hover:bg-green-300'>About</Link>
+
+                            <Link href={route('contact')} class='mx-1 hover:bg-green-300'>Contact</Link>
+                        </>
+                    </div>
+                </div>
+
+                {/* <div>
+                    TODO - APPLY THIS LATER
+                    {auth.user ? (
+                            <Link href={route('dashboard')} className='p-2 mx-1 absolute bottom-0 right-0 border-solid border border-black hover:bg-green-300'>
+                                Dashboard
+                            </Link>
+                    ) : (
+                        <>  
+                            <Link href={route('register')} className='p-2 mx-1 border-solid border border-black hover:bg-green-300'>
+                                Create
+                            </Link>
+
+                            <Link href={route('login')} className='p-2 mx-1 border-solid border border-black hover:bg-green-300'>
+                                Login
+                            </Link>
+                        </>
+                    )}
+                </div> */}
+                
+            </div>
+        </>
+    )
+}
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,49 +1,13 @@
-import { Link, Head } from '@inertiajs/react';
+import { Head } from '@inertiajs/react';
 
-export default function Home({ auth }) {
+import NavBar from '../Layouts/NavOverlay';
+
+export default function Home() {
     return (
         <>
-            <Head title="Home" />
+            <Head title="Home"/>
 
-            {/* This would be the overlay */}
-            <div class="p-4 flex flex-row justify-evenly items-center border border-solid border-black bg-blue-300">
-                <Link href={route('/')} class="m-1 font-bold underline">Majestic Miles</Link>
-
-                <div class="flex flex-col border border-solid border-black">
-                    <div class="flex flex-row">
-                        <a href="mailto:info.majesticmiles@gmail.com" class='m-1 hover:bg-green-300'>info.majesticmiles@gmail.com</a>
-                    </div>
-                    <div class="flex flex-row">
-                        <>
-                            <Link href={route('services')} class='m-1 hover:bg-green-300'>Services</Link>
-
-                            <Link href={route('about')} class='m-1 hover:bg-green-300'>About</Link>
-
-                            <Link href={route('contact')} class='m-1 hover:bg-green-300'>Contact</Link>
-                        </>
-                    </div>
-                </div>
-
-                {/* <div>
-                    TODO - APPLY THIS LATER
-                    {auth.user ? (
-                            <Link href={route('dashboard')} className='p-2 m-1 absolute bottom-0 right-0 border-solid border border-black hover:bg-green-300'>
-                                Dashboard
-                            </Link>
-                    ) : (
-                        <>  
-                            <Link href={route('register')} className='p-2 m-1 border-solid border border-black hover:bg-green-300'>
-                                Create
-                            </Link>
-
-                            <Link href={route('login')} className='p-2 m-1 border-solid border border-black hover:bg-green-300'>
-                                Login
-                            </Link>
-                        </>
-                    )}
-                </div> */}
-                
-            </div>
+            <NavBar/>
         </>
     )
 }
